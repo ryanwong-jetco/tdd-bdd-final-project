033ea1ad5750ec0fdd225332f6531bbcbfacd4c5
ryanwong-jetco <ryanwong@jetco.com.hk>
2025-07-03 19:09:24 +0800

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -231,7 +231,24 @@ class TestProductRoutes(TestCase):
         # check the data just to be sure
         for product in data:
             self.assertEqual(product["name"], test_name)
-        
+
+    def test_query_by_category(self):
+        """It should Query Products by category"""
+        products = self._create_products(10)
+        category = products[0].category
+        found = [product for product in products if product.category == category]
+        found_count = len(found)
+        logging.debug("Found Products [%d] %s", found_count, found)
+
+        # test for available
+        response = self.client.get(BASE_URL, query_string=f"category={category.name}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), found_count)
+        # check the data just to be sure
+        for product in data:
+            self.assertEqual(product["category"], category.name)
+          
     ######################################################################
     # Utility functions
     ######################################################################
